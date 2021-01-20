5edfd120a3c786d9afd91b7db36b6f0294e709cc
Adarsh Kumar <54638747+kadarsh2k00@users.noreply.github.com>
2021-01-20 23:28:15 +0530

    Initial Commit

--- carzone/settings.py
+++ carzone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@#$93=50=$q4q&_+crrf#5_gr92(vj2gqvga=(@-gq)014)kw0'
+SECRET_KEY = '9gs)3*15)l#wsa6v1guz6c@v$5iysym0cgmibnyhv-rk0s1+ly'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
